bf2234f41ee19db9286939a8bbc5d3805c0a894a
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-22 23:28:58 -0500

    feat: working on login function

--- js/main-page.js
+++ js/main-page.js
@@ -22,3 +22,60 @@ let searchDatabase = () => {
 $('#search-button').click(function () {
     searchDatabase();
 })
+
+
+
+// login modal function
+// click "Sign in to your account" button
+$('.btn-login').click((e) => {
+    e.preventDefault();
+
+    // get email and password
+    let email = $('#username').val(),
+        password = $('#sign_in_form_password').val();
+
+    // do an ajax request to get id
+    $.ajax("https://vet-codeathon.herokuapp.com/users/", {
+        type: "POST",
+        data: {
+            "email": email,
+            "password": password
+        }
+    }).done((data) => {
+        console.log(data);
+        // check if the email and password are in the users database
+        if (data !== []) {
+            // get the user id
+            // go to main-two page
+            window.location = 'main-page-two.html';
+        } else {
+            $.ajax("https://vet-codeathon.herokuapp.com/employees/", {
+                type: "POST",
+                data: {
+                    "email": email,
+                    "password": password
+                }
+            }).done((results) => {
+                console.log(results);
+                if (results === []) {
+                    alert("Sorry, you may enter the wrong email or password. You can try again or sign up.")
+                } else {
+                    // get employee_id and business_id
+                }
+            }).fail(() => alert("Sorry, something went wrong... Please try again later."));
+        }
+    }).fail(() => alert("Sorry, something went wrong... Please try again later."));
+
+
+    fetch('https://vet-codeathon.herokuapp.com/businesses/{id}')
+    .then(response => response.json())
+    .then(business => {
+        console.log(business);
+        // go to business profile page
+        // window.location = 'business profile page';
+    });
+
+    ////////// need to take update
+    // go to main-two page
+    window.location = 'main-page-two.html';
+});
